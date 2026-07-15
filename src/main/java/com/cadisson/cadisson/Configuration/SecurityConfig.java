91a1f27a04f77956e1f5fadd931d732b6d8a9de3
JE2M <2024207017.est@unaj.edu.pe>
2026-07-14 23:42:03 -0500

    Eliminar ruta antigua de usuarios de la configuracion de seguridad

--- src/main/java/com/cadisson/cadisson/Configuration/SecurityConfig.java
+++ src/main/java/com/cadisson/cadisson/Configuration/SecurityConfig.java
@@ -78,7 +78,6 @@ public class SecurityConfig {
                                 "/admin/**",
                                 "/eventos/**",
                                 "/servicios/**",
-                                "/usuarios/**",
                                 "/cotizaciones/**"
                         )
                         .hasRole("ADMIN")
